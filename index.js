af268f8711adedcc59b5a7705d5c446d09f27179
Akhil TJ <100039517+akhiltj012@users.noreply.github.com>
2022-10-12 22:24:21 +0530

    delete

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ function click(event){
 
    let t = document.querySelector("#time").value
 
-   let use = {"name":n,"email":e,"phone":p,"time":t}
-  let arr = [] || JSON.parse(localStorage.getItem("user"));
+  
+  let arr =  JSON.parse(localStorage.getItem("user")) || [];
+  let use = {"name":n,"email":e,"phone":p,"time":t}
   arr.push(use)
-   localStorage.setItem("user",JSON.stringify(use))
+   localStorage.setItem('user',JSON.stringify(arr))
+  
    console.log(arr)
+
+   for(let i=0;i<arr.length;i++){
+    let div = document.createElement("div")
+    div.setAttribute("id","arr")
+    document.querySelector("#inside").append(div)
+    div.innerText= `Name:- ${arr[i].name}` + `Phone:- ${arr[i].phone}`+ `Email:- ${arr[i].email}`+ `Date:- ${arr[i].time}`
+    console.log(arr[i].name);
+    let btn1 = document.createElement("button")
+    btn1.innerText="Delete";
+    div.append(btn1)
+   }
 }
