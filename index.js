3840b33b3b05e37fd7694c11aaf4ee5d6062ba12
Akhil TJ <100039517+akhiltj012@users.noreply.github.com>
2022-10-13 17:12:58 +0530

    call,apply,bind

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
-var btn = document.querySelector("#btn")
-
-btn.addEventListener("click",click)
-
-function click(event){
-
-   event.preventDefault();
-
-   let n = document.querySelector("#name").value
-
-   let e = document.querySelector("#email").value
-
-   let p = document.querySelector("#phone").value
-
-   let t = document.querySelector("#time").value
-
-  
-  let arr =  JSON.parse(localStorage.getItem("user")) || [];
-  let use = {"name":n,"email":e,"phone":p,"time":t}
-  arr.push(use)
-   localStorage.setItem('user',JSON.stringify(arr))
-  
-   console.log(arr)
-
-   for(let i=0;i<arr.length;i++){
-    let div = document.createElement("div")
-    div.setAttribute("id","arr")
-    document.querySelector("#inside").append(div)
-    div.innerText= `Name:- ${arr[i].name}` + `Phone:- ${arr[i].phone}`+ `Email:- ${arr[i].email}`+ `Date:- ${arr[i].time}`
-    console.log(arr[i].name);
-    let btn1 = document.createElement("button")
-    btn1.innerText="Delete";
-    div.append(btn1)
-   }
+var obj = {num:2}
+var add=function (a,b,c) {
+    return this.num + a+b+c
 }
+var arr=[1,2,3]
+  
+   console.log(add.call(obj,1,2,3));
+   console.log(add.apply(obj,arr));
+   console.log(add.bind(obj)(1,2,3));
